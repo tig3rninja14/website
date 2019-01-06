f0d65dd9bcbf3e0010c3a20d56f42d7a3ef772bd
BrianEnergy <42158290+BrianEnergy@users.noreply.github.com>
2019-01-06 17:10:55 -0600

    removed extra console.log information

--- carson.js
+++ carson.js
@@ -4,7 +4,7 @@ var answer = [70, 85, 67, 75, 89, 79, 85, 67, 65, 82, 83, 79, 78, 13];
 
 document.addEventListener('keydown', function(event){
 	easterEgg.push(event.keyCode);
-    console.log(easterEgg);
+    //console.log(easterEgg);
 
     if (JSON.stringify(easterEgg) === JSON.stringify(answer)) {
     	alert("YOU FUCKING FOUND THIS FUCKING EASTER EGG GOOD FUCKING JOB YOU DUMB FUCKING FUCKER! :) ENJOY $0 OFF OF YOUR NEXT ORDER.");
