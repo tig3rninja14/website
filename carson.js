05fa181e6c8e172023414da6b053e99e4ce686d3
BrianEnergy <42158290+BrianEnergy@users.noreply.github.com>
2019-01-23 19:05:06 -0600

    User friendly memes!

--- carson.js
+++ carson.js
@@ -2,14 +2,14 @@
 var easterEgg = [];
 // offercode-carson
 var answer = [79, 70, 70, 69, 82, 67, 79, 68, 69, 189, 67, 65, 82, 83, 79, 78, 13];
-var audio = new Audio('sounds/qUCIK bRIAN 8^2 (REEE I BELONG TO you yeah yeah yeah (ft. Joey on practice pad)).wav');
+var audio = new Audio('sounds/qUCIK bRIAN 8^2 (REEE I BELONG TO you yeah yeah yeah (ft. Joey on practice pad(NO CUSSING))).mp3');
 
 document.addEventListener('keydown', function(event){
 	easterEgg.push(event.keyCode);
 
     if (JSON.stringify(easterEgg) === JSON.stringify(answer)) {
     	audio.play();
-    	alert("YOU FUCKING FOUND THIS FUCKING OFFER-CODE!!! GOOD FUCKING JOB YOU DUMB FUCKING FUCKER!!! :) ENJOY $0 OFF OF YOUR NEXT ORDER!!!");
+    	alert("YOU FLIPPING FOUND THIS FLIPPING OFFER-CODE!!! GOOD FLIPPING JOB YOU SMART FLIPPING FLIPPER!!! :) ENJOY $0 OFF OF YOUR NEXT FATHERFLIPPING ORDER!!!");
     }
 
     if (event.keyCode === 13) {
